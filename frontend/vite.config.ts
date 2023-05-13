59fbd57a2c687679a8fc208d14a808ea618e96bf
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-05-13 12:18:45 +0200

    setup tailwind, eslint and prettier for frontend project
    Closes #71

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+  },
+});
