855a63315cfbae01259b656039cde055711cb3c0
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-05-13 15:17:16 +0200

    feature: setup routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,60 +1,16 @@
-import { UserManager, WebStorageStateStore } from "oidc-client-ts";
-import React, { useEffect, useMemo, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import { RouterProvider } from "react-router-dom";
 
-import authConfig from "./authConfig";
-import Callback from "./components/Callback";
-import Login from "./components/Login";
+import { router } from "./lib/siteRoutes";
 
-function App() {
-  const userManager = useMemo(() => {
-    return new UserManager({
-      userStore: new WebStorageStateStore({ store: window.localStorage }),
-      ...authConfig,
-    });
-  }, []);
-
-  function authorize() {
-    userManager.signinRedirect({ state: "a2123a67ff11413fa19217a9ea0fbad5" });
-  }
-
-  function clearAuth() {
-    userManager.signoutRedirect();
-  }
-
-  const [authenticated, setAuthenticated] = useState<boolean | null>(null);
-  const [userInfo, setUserInfo] = useState(null);
-
-  useEffect(() => {
-    userManager.getUser().then((user) => {
-      if (user) {
-        setAuthenticated(true);
-      } else {
-        setAuthenticated(false);
-      }
-    });
-  }, [userManager]);
-
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login auth={authenticated} handleLogin={authorize} />} />
-        <Route
-          path="/callback"
-          element={
-            <Callback
-              auth={authenticated}
-              setAuth={setAuthenticated}
-              userInfo={userInfo}
-              setUserInfo={setUserInfo}
-              handleLogout={clearAuth}
-              userManager={userManager}
-            />
-          }
-        />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </>
   );
-}
+};
 
 export default App;
