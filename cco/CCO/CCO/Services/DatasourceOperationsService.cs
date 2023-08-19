9298435465a03ef8dbbe482a451c8525f9ca0dc3
vita-ride <viktornovakovic77@gmail.com>
2023-08-19 23:36:13 +0200

    Datasource operations endpoints

---
+++ cco/CCO/CCO/Services/DatasourceOperationsService.cs
@@ -0,0 +1,42 @@
+﻿using Grpc.Core;
+
+namespace CCO.Services
+{
+    public class DatasourceOperationsService : DatasourceOperations.DatasourceOperationsBase
+    {
+         public override Task<DatabaseReadResponse> DatabaseRead (DatabaseReadRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<DatabaseWriteResponse> DatabaseWrite (DatabaseWriteRequest request, ServerCallContext context) 
+        { 
+            throw new NotImplementedException(); 
+        }
+
+        public override Task<DatabaseDeleteResponse> DatabaseDelete(DatabaseDeleteRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<CacheReadResponse> CacheRead (CacheReadRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<CacheWriteResponse> CacheWrite (CacheWriteRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<QueueReadResponse> QueueRead (QueueReadRequest request, ServerCallContext context) 
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<QueueWriteResponse> QueueWrite (QueueWriteRequest request, ServerCallContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
