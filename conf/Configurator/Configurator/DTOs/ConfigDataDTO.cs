65910b209666a1585393c375f14d4bef5982078b
vita-ride <viktornovakovic77@gmail.com>
2023-08-22 19:40:07 +0200

    Add controller for RP and CCO

---
+++ conf/Configurator/Configurator/DTOs/ConfigDataDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace Configurator.DTOs
+{
+    public class ConfigDataDTO
+    {
+        public string Data { get; set; }
+        public string ValidFrom { get; set; }
+
+        public ConfigDataDTO(string data, string validFrom)
+        {
+            Data = data;
+            ValidFrom = validFrom;
+        }
+    }
+}
---
+++ conf/Configurator/Configurator/DTOs/ConfigMetadataDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Configurator.DTOs
+{
+    public class ConfigMetadataDTO
+    {
+        public string ApiName { get; set; }
+        public string ApiVersion { get; set; }
+        public string? BasePath { get; set; }
+        public ConfigMetadataDTO(string apiName, string apiVersion, string? basePath)
+        {
+            ApiName = apiName;
+            ApiVersion = apiVersion;
+            BasePath = basePath;
+        }
+    }
+}
