aa22bd7d0fa612e3ebd2587ec273b03717999c6a
Luka Jovicic <11800546+luka-j@users.noreply.github.com>
2023-08-28 00:47:20 +0200

    #87: Add config initialization logic

---
+++ rp/ApiGatewayRequestProcessor/ApiGatewayRequestProcessor/Initializer.cs
@@ -0,0 +1,35 @@
+﻿using ApiGatewayRequestProcessor.Configs;
+using ApiGatewayRequestProcessor.Gateways;
+
+namespace ApiGatewayRequestProcessor;
+
+public class Initializer
+{
+
+    private Serilog.ILogger _logger = Serilog.Log.Logger;
+    
+    public Initializer(ConfGateway confGateway, ConfigRepository configRepository)
+    {
+        try
+        {
+            _logger.Information("Initializing configs");
+            var specs = confGateway.GetSpecs();
+            foreach (var spec in specs.Specs_)
+            {
+                configRepository.UpdateConfig(spec.Data, DateTime.Now);
+            }
+            _logger.Information("Initialized {Count} configs", specs.Specs_.Count);
+        }
+        catch (Exception)
+        {
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+            {
+                _logger.Warning("Failed to initialize configs");
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
+}
